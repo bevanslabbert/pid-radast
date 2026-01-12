40a8839f371d7885325ee694a6a2edee496ee357
EUZABS <89096655+bevanslabbert@users.noreply.github.com>
2026-01-12 22:22:39 +0200

    Update main.py

--- classifier-guided-physics-informed-diffusion/main.py
+++ classifier-guided-physics-informed-diffusion/main.py
@@ -64,7 +64,7 @@ def main():
         transforms.GaussianBlur(3),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
+    ])
 
     diffusion_transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
@@ -73,7 +73,7 @@ def main():
         transforms.Grayscale(num_output_channels=3),  # Convert 1 channel → 3 channels
         transforms.ToTensor(),
         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-    ]),
+    ])
 
     trainloader, valloader, testloader = get_data_loaders(
         args.dataset or "Mirabest",
