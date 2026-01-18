611e4256b685a519acc1a1baa57250877c377591
EUZABS <89096655+bevanslabbert@users.noreply.github.com>
2026-01-18 20:27:37 +0200

    Update train_pipeline.py

--- classifier-guided-physics-informed-diffusion/src/pipelines/train_pipeline.py
+++ classifier-guided-physics-informed-diffusion/src/pipelines/train_pipeline.py
@@ -131,8 +131,8 @@ def train_diffusion(config, trainloader, device, result_directory, resume, check
     # --- UNet that supports class conditioning ---
     unet = UNet2DConditionModel(
         sample_size=32,
-        in_channels=3,
-        out_channels=3,
+        in_channels=1,
+        out_channels=1,
         layers_per_block=2,
         block_out_channels=(64, 64, 128, 256),
         down_block_types=("DownBlock2D", "DownBlock2D", "AttnDownBlock2D", "DownBlock2D"),
