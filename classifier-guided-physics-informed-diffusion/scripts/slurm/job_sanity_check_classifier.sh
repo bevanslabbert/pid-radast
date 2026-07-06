0358575b6ac0d11a4e799dddd651895c9d80ab1a
EUZABS <89096655+bevanslabbert@users.noreply.github.com>
2026-07-06 08:35:46 +0200

    more samples

--- classifier-guided-physics-informed-diffusion/scripts/slurm/job_sanity_check_classifier.sh
+++ classifier-guided-physics-informed-diffusion/scripts/slurm/job_sanity_check_classifier.sh
@@ -18,5 +18,5 @@ source .venv/bin/activate
 python scripts/sanity_check_classifier.py \
     --config config/classification.yaml \
     --dataset ${DATASET:-mirabest} \
-    --num-samples ${NUM_SAMPLES:-10} \
+    --num-samples ${NUM_SAMPLES:-100} \
     --output results/sanity_check
